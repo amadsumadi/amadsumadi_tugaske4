d3077b53bfcfa8cdddb07c18d444def590795338
amad sumadi <amadsumadi067@gmail.com>
2021-11-02 17:12:34 +0700

    menambahkan file belajarpython2.py

---
+++ belajarpython2.py
@@ -0,0 +1,20 @@
+print('assalamualaikum')
+print('apa kabar semua?')
+print('kali ini saya akan memberikan petunjuk bagaimana menghitung luas dan keliling lingkaran')
+print(' ')
+print('Lingkaran adalah bentuk yang terdiri dari semua titik dalam bidang yang berjarak tertentu dari titik tertentu,')
+print('pusat; ekuivalennya adalah kurva yang dilacak oleh titik yang bergerak dalam bidang sehingga jaraknya dari titik tertentu adalah konstan.')
+print('Jarak antara titik mana pun dari lingkaran dan pusat disebut jari-jari')
+print('')
+print('1. Cara mencari luas lingkaran')
+print('Luas lingkaran (L) = πr2 atau π x r x r')
+print('Penentuan nilai π menggunakan 22/7 atau 3,14,')
+print('ditentukan ketika ada angka yang diketahui terlebih dahulu,')
+print('Jika angka itu termasuk kelipatan 7 maka menggunakan 22/7,')
+print('sementara jika tidak termasuk kelipatan 7 berarti menggunakan 3,14.')
+print(' ')
+print('2. Cara Mencari Keliling Lingkaran.')
+
+
+
+
