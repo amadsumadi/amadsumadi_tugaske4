54c4ea777602eec0fd6087a985b16508a7f45f28
amad sumadi <amadsumadi067@gmail.com>
2021-11-02 17:39:05 +0700

    menambahkan file tugas ke 4

---
+++ program_untuk_mencari_luas_dan_keliling_lingkaran.py
@@ -0,0 +1,9 @@
+print("Program untuk mencari Luas dan Keliling Lingkaran ")
+r = int(input("Masukkan jari-jari = "))
+
+phi = 22/7
+Luas = phi*r*r
+Keliling = 2*phi*r
+
+print("Luas lingkaran Adalah       =", Luas)
+print("Keliling Lingkaran Adalah   =", Keliling)
