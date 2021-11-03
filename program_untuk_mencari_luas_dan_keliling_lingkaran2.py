2aba804efd2075829985f751561851e75bdd0546
amad sumadi <amadsumadi067@gmail.com>
2021-11-03 17:05:32 +0700

    menambahkan file vscode1

---
+++ program_untuk_mencari_luas_dan_keliling_lingkaran2.py
@@ -0,0 +1,11 @@
+#Tugas Ke 4 Bahasa Pemrograman
+##Cara mencari luas dan keliling lingkaran
+
+###1. Cara mencari luas lingkaran
+Luas lingkaran (L) = πr2 atau π x r x r
+Penentuan nilai π menggunakan 22/7 atau 3,14,
+ditentukan ketika ada angka yang diketahui terlebih dahulu,
+Jika angka itu termasuk kelipatan 7 maka menggunakan 22/7,
+sementara jika tidak termasuk kelipatan 7 berarti menggunakan 3,14.
+
+####2. Cara Mencari Keliling Lingkaran.
